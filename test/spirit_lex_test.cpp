102e984d6ae7913b7f8ec1af66e2cb396f801cbf
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-03-20 08:54:30 +0900

    added a test case for Spirit.Lex.

--- test/spirit_lex_test.cpp
+++ test/spirit_lex_test.cpp
@@ -177,3 +177,88 @@ TEST_CASE("Spirit.Lex getting default token info with white spaces", "[lexer]")
     REQUIRE((std::get<0>(tokenStore[1]) == '+'));
     REQUIRE((std::get<1>(tokenStore[1]) == "+"));
 }
+
+//==============================================================================
+namespace
+{
+    enum token_id
+    {
+        ID_PLUS = 1024,
+        ID_MINUS,
+        ID_MUL,
+        ID_DIV,
+        ID_LPAREN,
+        ID_RPAREN
+    };
+
+    template <typename Lexer>
+    struct op_token_with_explicit_token_id : lex::lexer<Lexer>
+    {
+        op_token_with_explicit_token_id()
+        {
+            this->self.add
+                        ('+', ID_PLUS)
+                        ('-', ID_MINUS)
+                        ('*', ID_MUL)
+                        ('/', ID_DIV)
+                        ('(', ID_LPAREN)
+                        (')', ID_RPAREN);
+        }
+    };
+}   // un-named namespace
+
+TEST_CASE("Spirit.Lex getting token info with explicit token id", "[lexer]")
+{
+    char const * pBegin = "+-*/()"; // no spaces in the source string.
+    char const * pEnd = pBegin + std::strlen(pBegin);
+
+    using lexer_t = lex::lexertl::lexer<>;
+    using token_store_t = std::vector<std::pair<int, std::string>>;
+
+    op_token_with_explicit_token_id<lexer_t> tokens;
+    token_store_t tokenStore;
+
+    REQUIRE(
+        lex::tokenize(
+                pBegin, pEnd,
+                tokens,
+                [&tokenStore](auto const& token) {
+                    static_assert(
+                        std::is_same<
+                                lex::lexertl::token<>,
+                                std::decay_t<decltype(token)>
+                        >()
+                    );
+
+                    static_assert(
+                        std::is_same<
+                                boost::iterator_range<char const *>,
+                                std::decay_t<decltype(token.value())>
+                        >()
+                    );
+                    
+                    tokenStore.emplace_back(
+                        token.id(),
+                        std::string{ std::begin(token.value()), std::end(token.value()) }
+                    );
+                    
+                    return true;
+                }
+        )
+    );
+
+    REQUIRE(tokenStore.size() == 6);
+
+    REQUIRE((std::get<0>(tokenStore[0]) == ID_PLUS));
+    REQUIRE((std::get<1>(tokenStore[0]) == "+"));
+    REQUIRE((std::get<0>(tokenStore[1]) == ID_MINUS));
+    REQUIRE((std::get<1>(tokenStore[1]) == "-"));
+    REQUIRE((std::get<0>(tokenStore[2]) == ID_MUL));
+    REQUIRE((std::get<1>(tokenStore[2]) == "*"));
+    REQUIRE((std::get<0>(tokenStore[3]) == ID_DIV));
+    REQUIRE((std::get<1>(tokenStore[3]) == "/"));
+    REQUIRE((std::get<0>(tokenStore[4]) == ID_LPAREN));
+    REQUIRE((std::get<1>(tokenStore[4]) == "("));
+    REQUIRE((std::get<0>(tokenStore[5]) == ID_RPAREN));
+    REQUIRE((std::get<1>(tokenStore[5]) == ")"));
+}
