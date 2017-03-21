dcf48bd83d2e0dcaab99302f353dc256d635317c
Gil Ho, Jang <jmxexpert@hotmail.com>
2017-03-21 15:07:59 +0900

    defined simple calculator tokens.

--- src/calculator.h
+++ src/calculator.h
@@ -8,11 +8,25 @@
 namespace algovisu
 {
     namespace lex = boost::spirit::lex;
-    
+
     template <typename Lexer>
     struct calc_token : lex::lexer<Lexer>
     {
+        calc_token()
+        {
+            decimal_integer_ = R"(0|[1-9]\d*)";
 
+            this->self = decimal_integer_
+                            | '+' | '-'
+                            | '*' | '/'
+                            | '(' | ')';
+
+            white_space_ = R"(\s+)";
+            this->self("WS") = white_space_;
+        }
+
+        lex::token_def<> decimal_integer_;
+        lex::token_def<> white_space_;
     };
 } // namespace algovisu
 
